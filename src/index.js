980fd1c482aa8f3a19366e1409fb9945f4ef633d
Joseph <josephylee72@gmail.com>
2023-09-12 22:33:16 -0700

    completed tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const input = document.querySelector("#new-task-description");
+  const tasks = document.querySelector("#tasks");
+  const form = document.querySelector("#create-task-form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    if (input.value === "") return;
+    const toAdd = input.value;
+    const item = document.createElement("li");
+    item.textContent = toAdd;
+    tasks.append(item);
+    input.value = "";
+    input.focus();
+    item.onclick = function() {
+      item.remove();
+    }
+  }
 });
